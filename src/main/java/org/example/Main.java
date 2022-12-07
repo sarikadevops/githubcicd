18912106402905a42e8647a7775d7e7d34e60509
sarikadevops <sarikadhaygude2@gmail.com>
2022-12-07 10:44:53 +0530

    code

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+
+   // echo "# githubcicd" >> README.md
+   // git init
+   // git add README.md
+   // git commit -m "first commit"
+   // git branch -M main
+   // git remote add origin https://github.com/sarikadevops/githubcicd.git
+   // git push -u origin main
+   public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
